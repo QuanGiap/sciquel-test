03d62928e360c54a9b8ede7879f36d0cb4327a93
QuanGiap <minhquangiapnguyen@gmail.com>
2024-02-09 15:23:29 -0800

    Comment feature

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -1,13 +1,43 @@
+import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-
+import { commentGetSchema,commentPostSchema } from "./schema";
 
 export async function GET(req: NextRequest){
-    // fill here in
-
-    return NextResponse.json({ test: "success", }, { status: 200 })
+    const prisma = new PrismaClient();
+    const searchParams = req.nextUrl.searchParams;
+    let query:{[key:string]:Number} = {};
+    searchParams.forEach((value,key)=>{
+        query[key]=Number(value);
+    })
+    const {page=0,limit=50} = commentGetSchema.parse(query);
+    const comments = await prisma.comment.findMany({take:limit,skip:limit*page,orderBy:{
+        createdAt:"desc"
+    }});
+    return NextResponse.json({ test: "success", comments}, { status: 200 })
 }
 
 export async function POST(req: NextRequest){
-    // fill here in
+    const body = await req.json();
+    const {comment,email,name} = commentPostSchema.parse(body);
+    const prisma = new PrismaClient();
+    //find user
+    let user = await prisma.user.findFirst({
+        where:{
+            email,
+        },
+    })
+    // if user not exist create one
+    if(!user) user = await prisma.user.create({
+        data:{
+            email,
+            name,
+        }
+    })
+    const new_comment = await prisma.comment.create({
+        data:{
+            userId:user.id,
+            content:comment,
+        }
+    })
     return NextResponse.json({ test: "success", }, { status: 200 })
 }
--- src/app/api/comments/schema.ts
+++ src/app/api/comments/schema.ts
@@ -1,9 +1,12 @@
 import { z } from "zod";
 
 export const commentGetSchema = z.object({
-
+    page:z.number().optional(),
+    limit:z.number().optional(),
 })
 
 export const commentPostSchema = z.object({
-    
+    name:z.string(),
+    email:z.string().email(),
+    comment:z.string(),
 })
